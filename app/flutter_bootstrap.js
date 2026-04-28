35079290447b9ec9580367193dbd8eae8cc11a56
prodigeeee <christophek@hotmail.com>
2026-04-28 15:29:18 +0200

    Désactivation du Service Worker pour forcer les mises à jour

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -36,7 +36,7 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3661961598" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  config: {
+    // Désactivation du Service Worker pour forcer la mise à jour systématique
   }
 });
